0739c1f1d64424bfd42474b465b16f7fea8847fd
unknown <engel199702@hotmail.com>
2018-08-20 13:23:34 -0600

    base de datos actualizada

--- adminlte.sql
+++ adminlte.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 09, 2018 at 10:21 PM
+-- Generation Time: Aug 20, 2018 at 09:22 PM
 -- Server version: 10.1.33-MariaDB
 -- PHP Version: 7.2.6
 
@@ -21,6 +21,27 @@ SET time_zone = "+00:00";
 --
 -- Database: `adminlte`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `archivo`
+--
+
+CREATE TABLE `archivo` (
+  `id` int(11) NOT NULL,
+  `nombre` text,
+  `directorio` text
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `archivo`
+--
+
+INSERT INTO `archivo` (`id`, `nombre`, `directorio`) VALUES
+(17, '1534702575_4860.jpg', 'download/1534702575_4860.jpg'),
+(18, '1534787380_5611.jpg', 'download/1534787380_5611.jpg'),
+(19, '1534788484_5966.jpg', 'download/1534788484_5966.jpg');
 
 -- --------------------------------------------------------
 
@@ -48,7 +69,57 @@ CREATE TABLE `client` (
 --
 
 INSERT INTO `client` (`id`, `identificacion`, `tipoID`, `nombre`, `nombre_fantasia`, `telefono`, `email`, `provincia`, `canton`, `distrito`, `barrio`, `direccion`) VALUES
-(52, '116910920', 'FÃ­sica', 'Frederick Calderon Jimenez', 'Engel', '89871987', 'engel199702@hotmail.com', 'San JosÃ©', 'PÃ©rez ZeledÃ³n', 'Daniel Flores', 'Residencial Halder', 'San Jose 11903');
+(52, '116910920', 'FÃ­sica', 'Frederick Calderon Jimenez', 'Engel', '89871987', 'engel199702@hotmail.com', 'San JosÃ©', 'PÃ©rez ZeledÃ³n', 'Daniel Flores', 'Residencial Halder', 'San Jose 11903'),
+(54, '8789858256', 'JurÃ­dica', 'Pollos Locos', 'Pollos', '12523021', 'pollos@gmail.com', 'Heredia', 'Central', 'Merced', 'Uno', '1234 abc'),
+(55, '9009009', 'FÃ­sca', 'Lisa', 'Lissa', '52565485', 'lisa@mymail.com', 'Alajuela', 'Central', 'Carmen', 'Algun barrio', 'alguna direccion');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `factura`
+--
+
+CREATE TABLE `factura` (
+  `id` int(11) NOT NULL,
+  `id_cliente` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `factura`
+--
+
+INSERT INTO `factura` (`id`, `id_cliente`) VALUES
+(54, 52),
+(55, 52),
+(57, 52),
+(58, 54),
+(59, 55);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `factura_producto`
+--
+
+CREATE TABLE `factura_producto` (
+  `id` int(11) NOT NULL,
+  `id_factura` int(11) DEFAULT NULL,
+  `id_producto` int(11) DEFAULT NULL,
+  `cantidad` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `factura_producto`
+--
+
+INSERT INTO `factura_producto` (`id`, `id_factura`, `id_producto`, `cantidad`) VALUES
+(47, 54, 6, 1),
+(48, 55, 6, 1),
+(49, 57, 6, 1),
+(50, 58, 7, 1),
+(51, 58, 6, 1),
+(52, 59, 6, 1),
+(53, 59, 8, 1);
 
 -- --------------------------------------------------------
 
@@ -71,8 +142,9 @@ CREATE TABLE `producto` (
 --
 
 INSERT INTO `producto` (`id`, `codigo`, `nombre`, `precio_compra`, `precio_venta`, `impuesto_venta`, `cantidad_existente`) VALUES
-(6, '9878', 'Producto prueba', '15700', '20900', '13', 89),
-(7, '7889', 'Producto dos prueba', '7899', '9000', '15', 43);
+(6, '9878', 'Producto prueba', '15700', '20900', '13', 15),
+(7, '7889', 'Producto prueba 2', '7899', '9000', '15', 43),
+(8, '2020', 'Laptop', '500000', '700000', '15', 100);
 
 -- --------------------------------------------------------
 
@@ -99,6 +171,12 @@ INSERT INTO `users` (`id`, `name`, `email`, `password`) VALUES
 --
 
 --
+-- Indexes for table `archivo`
+--
+ALTER TABLE `archivo`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `client`
 --
 ALTER TABLE `client`
@@ -108,11 +186,28 @@ ALTER TABLE `client`
   ADD UNIQUE KEY `email` (`email`);
 
 --
+-- Indexes for table `factura`
+--
+ALTER TABLE `factura`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_cliente` (`id_cliente`);
+
+--
+-- Indexes for table `factura_producto`
+--
+ALTER TABLE `factura_producto`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_factura` (`id_factura`),
+  ADD KEY `id_producto` (`id_producto`);
+
+--
 -- Indexes for table `producto`
 --
 ALTER TABLE `producto`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `codigo` (`codigo`);
+  ADD UNIQUE KEY `codigo` (`codigo`),
+  ADD UNIQUE KEY `codigo_2` (`codigo`),
+  ADD UNIQUE KEY `codigo_3` (`codigo`);
 
 --
 -- Indexes for table `users`
@@ -126,22 +221,57 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `archivo`
+--
+ALTER TABLE `archivo`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
 -- AUTO_INCREMENT for table `client`
 --
 ALTER TABLE `client`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=53;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=56;
+
+--
+-- AUTO_INCREMENT for table `factura`
+--
+ALTER TABLE `factura`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=60;
+
+--
+-- AUTO_INCREMENT for table `factura_producto`
+--
+ALTER TABLE `factura_producto`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=54;
 
 --
 -- AUTO_INCREMENT for table `producto`
 --
 ALTER TABLE `producto`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `factura`
+--
+ALTER TABLE `factura`
+  ADD CONSTRAINT `factura_ibfk_1` FOREIGN KEY (`id_cliente`) REFERENCES `client` (`id`);
+
+--
+-- Constraints for table `factura_producto`
+--
+ALTER TABLE `factura_producto`
+  ADD CONSTRAINT `factura_producto_ibfk_1` FOREIGN KEY (`id_factura`) REFERENCES `factura` (`id`),
+  ADD CONSTRAINT `factura_producto_ibfk_2` FOREIGN KEY (`id_producto`) REFERENCES `producto` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
